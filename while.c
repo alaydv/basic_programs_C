dcf7ba9a4cbc02c310b30fa083ebb9bbf438ea23
alaydv <erickalay81194@gmail.com>
2023-03-20 14:47:42 -0500

    Loops with while

---
+++ while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int num = 0;
+
+int main(){
+    while(num < 10){
+        printf("Number is: %d\n", num);
+        num++;
+    }
+    printf("Bucle terminado");
+    return 0;
+}
